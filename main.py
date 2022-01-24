816164b1752bd988d49d0b574d5d0211ec540acd
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-24 12:53:38 -0800

    added the title and name

---
+++ main.py
@@ -0,0 +1,6 @@
+# Name: Ruben Sanduleac
+# Description: This is a number guessing game that gives the user the choice to pick the
+#              difficulty of the game.
+
+import random
+print("Welcome to the Number Guessing Game!")
