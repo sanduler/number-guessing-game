21563f0609a362df09dffcaa7ba78073fda9ec8e
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-24 13:54:41 -0800

    added a restart loop

--- main.py
+++ main.py
@@ -3,29 +3,42 @@
 #              difficulty of the game.
 
 import random
+from background import logo
 
 attempts = 0
 game_over = False
+run_again = True
+
+print(logo)
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
 
-diff = input("Choose a difficulty. Type 'easy' or 'hard': ")
-if diff == 'easy':
-    attempts = 10
-elif diff == 'hard':
-    attempts = 5
-number = random.randint(1, 100)
+while run_again:
+    print("I'm thinking of a number between 1 and 100.")
+    diff = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if diff == 'easy':
+        attempts = 10
+    elif diff == 'hard':
+        attempts = 5
+    number = random.randint(1, 100)
 
-while not game_over and not attempts == 0:
-    guess = int(input("Make a guess: "))
-    if number > guess:
-        print("Too low.")
-    elif number < guess:
-        print("Too high")
-    elif number == guess:
-        print("You Guessed It!")
-        break
+    while not game_over and not attempts == 0:
+        guess = int(input("Make a guess: "))
+        if number > guess:
+            print("Too low.")
+        elif number < guess:
+            print("Too high")
+        elif number == guess:
+            print("You Guessed It!")
+            break
+        else:
+            print("Invalid input.")
+
+        attempts -= 1
+        print(f"Attempts Left [{attempts}] ")
+    print("Sorry you ran out of attempts.")
+    run_again = input(
+        "Do you want to try again: Type 'y' yes and 'n' for no: ").lower()
+    if run_again == 'y':
+        run_again = True
     else:
-        print("Invalid input.")
-
-    attempts -= 1
+        run_again = False
